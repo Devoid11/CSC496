817647e0849c3952fb0a03deb2403be1b9648c57
root <root@Perseverance.localdomain>
2021-10-17 21:18:08 -0400

    Overcame error involving reinstalling the gatsby-sharp plugin, then connected site to wordpress via GraphQL query

--- my-gatsby-site/gatsby-config.js
+++ my-gatsby-site/gatsby-config.js
@@ -6,11 +6,45 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     {
+
       resolve: "gatsby-source-filesystem",
       options: {
         name: `blog`,
-        path: `${__dirname}/blog`,
+        path: `${__dirname}/src/pages/blog.js`,
       }
     },
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/src/pages/blog.js`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
+      },
+    },
+
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        url: 'https://csc496wordpress.tldr.dev/graphql',
+        protocol: 'https',
+        hostingWPCOM: false,
+        useACF: false
+      }
+    },
+
+    /*
+     {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        baseUrl: `wpexample.com`,
+        protocol: `https`,
+        hostingWPCOM: false,
+        useACF: false
+      }
+    },
+*/
+
+
   ],
 };
